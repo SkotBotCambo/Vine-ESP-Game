a2ff4a701d82112c5217dc01917bed273c0ed4ab
scottofthescience <scottallencambo@gmail.com>
2014-10-04 20:16:49 -0400

    can get vine video, still need to figure out looping and controlling

--- app/route.py
+++ app/route.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template
+import requests
+import json
+
 app = Flask(__name__)
+
+# vine stuff
+
+global video_urls
+video_urls = []
+vine_url = 'https://api.vineapp.com/timelines/popular'
+
+def get_video_urls():
+    global video_urls
+    r = requests.get(vine_url)
+    video_urls = urls = [str(x['videoLowURL']) for x in r.json()['data']['records']]
 
 @app.route('/')
 def main():
     return render_template("layout.html")
 
+@app.route('/get_vine/<id>')
+def get_vine(id):
+   return video_urls[int(id)]
+
 if __name__ == '__main__':
+    get_video_urls()
     app.debug = True
     app.run()
 
--- app/static/js/esp-game.js
+++ app/static/js/esp-game.js
@@ -1,19 +1,27 @@
-var vine_url = "https://api.vineapp.com/timelines/popular";
-var video_num = 0; // current index of vine records
-var vine_json; // all data for pulling vine video urls;
-$( document ).ready(function() {
-    console.log("page loaded");
+
+
+function get_vine(i){
+    $.get('/get_vine/' + i, function( data ){ 
+        $("iframe.vine-embed").attr("src", data);
+        });
+    /*
     $.ajax({
-        url: vine_url,
+        url: '/get_vine/' + video_num,
         type: 'GET',
-        //headers:
-            
         success:function( result ){
             
             if(result['success']){
                 vine_json = result;
-                $("iframe.vine-embed").attr("src", vine_json['data']['records'][0]['videoLowURL']);
+                $("iframe.vine-embed")
+                    .attr("src", vine_json['data']['records'][0]['videoLowURL']);
             }
         }
     });
+    */
+}
+
+
+$( document ).ready(function() {
+    console.log("page loaded");
+    get_vine(0);
 });
