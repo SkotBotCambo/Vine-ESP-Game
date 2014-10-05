363795fea5481849a82d31c75d6e6c0b5d8f5100
scottofthescience <scottallencambo@gmail.com>
2014-10-04 21:09:09 -0400

    addded countdown timer, fixed the way vine is working

--- app/static/js/esp-game.js
+++ app/static/js/esp-game.js
@@ -1,27 +1,27 @@
-
+var video_num = 0; // keeps track of number of attempts
 
 function get_vine(i){
-    $.get('/get_vine/' + i, function( data ){ 
-        $("iframe.vine-embed").attr("src", data);
-        });
-    /*
-    $.ajax({
-        url: '/get_vine/' + video_num,
-        type: 'GET',
-        success:function( result ){
-            
-            if(result['success']){
-                vine_json = result;
-                $("iframe.vine-embed")
-                    .attr("src", vine_json['data']['records'][0]['videoLowURL']);
-            }
-        }
-    });
-    */
+    $.get('/get_vine/' + i, function( data ){
+        console.log("url recieved"); 
+        $("video#vine_vid").attr("src", data);
+      });
 }
 
 
 $( document ).ready(function() {
     console.log("page loaded");
-    get_vine(0);
+    get_vine(video_num);
+
+    $( "button#timer" ).click(function () {
+        console.log("player ready");
+        var t = document.getElementById('timer');
+        var now = new Date();
+        deadline = new Date(now.getTime() + 3*1000);
+        t.innerHTML = countdown(deadline).toString();
+        setInterval(function(){
+            t.innerHTML = countdown(deadline).toString();
+        }, 1000);
+    });
+ 
 });
+
